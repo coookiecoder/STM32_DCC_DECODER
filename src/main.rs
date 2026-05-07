61785cea10fe1bd999040354222fc861fc1a17c0
Ambroise BAREUX <ambroise.bareux@gmail.com>
2026-05-07 19:08:52 +0200

    tried to make it work with F303X88

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,16 @@
 
 use panic_halt;
 
-use stm32f4xx_hal as hal;
+use stm32f3xx_hal as hal;
 
 use hal::pac;
 
-use hal::prelude::_stm32f4xx_hal_rcc_RccExt;
-use hal::prelude::_stm32f4xx_hal_gpio_GpioExt;
-use hal::prelude::_fugit_RateExtU32;
+use hal::prelude::*;
 
-use hal::rcc::Config;
-
-use hal::hal_02::digital::v2::InputPin;
-use hal::hal_02::digital::v2::OutputPin;
-
-use hal::spi::SpiSlave;
-use hal::spi::Mode;
-use hal::spi::Phase;
-use hal::spi::Polarity;
+use hal::spi::{Mode, Polarity, Phase, Spi};
+use hal::spi::config::Config as SpiConfig;
+use hal::hal::digital::v2::InputPin;
+use hal::hal::digital::v2::OutputPin;
 
 pub fn fake_exit() -> ! {
     loop {
@@ -41,76 +34,80 @@ const DECODED_DATA_SIZE: usize = 64;
 
 #[entry]
 fn main() -> ! {
-    let _dp = pac::Peripherals::take().expect("cannot take peripherals");
-    let mut _cp = pac::CorePeripherals::take().expect("cannot take core peripherals");
+    let dp = pac::Peripherals::take().expect("cannot take peripherals");
+    let mut cp = pac::CorePeripherals::take().expect("cannot take core peripherals");
 
-    let config = Config::hse(25.MHz()).sysclk(84.MHz()).require_pll48clk();
-    let mut rcc = _dp.RCC.freeze(config);
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-    _cp.DCB.enable_trace();
-    _cp.DWT.enable_cycle_counter();
+    let clocks = rcc.cfgr.sysclk(72.MHz()).freeze(&mut flash.acr);
 
-    let _gpio_a = _dp.GPIOA.split(&mut rcc);
-    let _gpio_b = _dp.GPIOB.split(&mut rcc);
-    let _gpio_c = _dp.GPIOC.split(&mut rcc);
-    let _gpio_d = _dp.GPIOD.split(&mut rcc);
-    let _gpio_e = _dp.GPIOE.split(&mut rcc);
+    cp.DCB.enable_trace();
+    cp.DWT.enable_cycle_counter();
 
-    let dcc_input = _gpio_a.pa1.into_pull_up_input();
-    let mut pico_output = _gpio_a.pa2.into_push_pull_output();
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
 
-    let spi_clock = _gpio_a.pa5.into_alternate();
-    let master_in_slave_out = _gpio_a.pa6.into_alternate();
-    let master_out_slave_in = _gpio_a.pa7.into_alternate();
-    let negative_slave_select = _gpio_a.pa4.into_alternate();
+    let dcc_input = gpioa.pa1.into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr);
+    let mut pico_output = gpioa.pa2.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+
+    // Use into_af_push_pull with AF5 for SPI1
+    let spi_clock = gpioa.pa5.into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+    let master_in_slave_out = gpioa.pa6.into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+    let master_out_slave_in = gpioa.pa7.into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
+    let _negative_slave_select = gpioa.pa4.into_af_push_pull::<5>(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrl);
 
     let mode = Mode {polarity: Polarity::IdleLow, phase: Phase::CaptureOnFirstTransition};
+    let spi_config = SpiConfig::default().mode(mode);
 
-    let mut spi = SpiSlave::new(_dp.SPI1, (Some(spi_clock), Some(master_in_slave_out), Some(master_out_slave_in), Some(negative_slave_select)), mode, &mut rcc);
+    let mut spi = Spi::new(dp.SPI1, (spi_clock, master_in_slave_out, master_out_slave_in), spi_config, clocks, &mut rcc.apb2);
 
-    let mut bits:usize = 0;
-    let mut byte:usize = 0;
+    // Force slave mode by clearing MSTR bit in CR1
+    unsafe {
+        (*pac::SPI1::ptr()).cr1.modify(|_, w| w.mstr().slave());
+    }
 
     let mut decoded_data = [0u8; DECODED_DATA_SIZE];
-    let mut preamble_size:usize = 0;
 
     loop {
-        while get_dcc_data(&dcc_input, &_cp.DWT) {
+        let mut preamble_size: usize = 0;
+        while get_dcc_data(&dcc_input, &cp.DWT) {
             preamble_size += 1;
         }
 
-        while preamble_size > 9 && preamble_size < 24 {
-            if get_dcc_data(&dcc_input, &_cp.DWT) {
-                decoded_data[byte] |= 1 << (7 - bits);
-            } else {
-                decoded_data[byte] &= !(1 << (7 - bits));
-            }
+        if preamble_size >= 10 {
+            let mut byte: usize = 0;
+            let mut bits: usize = 0;
+            decoded_data.fill(0);
 
-            bits += 1;
-
-            if bits >= 8 {
-                if get_dcc_data(&dcc_input, &_cp.DWT) == false {
-                    byte += 1;
+            loop {
+                if get_dcc_data(&dcc_input, &cp.DWT) {
+                    decoded_data[byte] |= 1 << (7 - bits);
                 } else {
-                    byte = DECODED_DATA_SIZE;
+                    decoded_data[byte] &= !(1 << (7 - bits));
                 }
-                bits = 0;
+
+                bits += 1;
+
+                if bits >= 8 {
+                    byte += 1;
+                    bits = 0;
+
+                    // Packet End Bit (1) or Data Byte Separator (0)
+                    if get_dcc_data(&dcc_input, &cp.DWT) {
+                        // End of packet
+                        break;
+                    }
+                }
+
+                if byte >= DECODED_DATA_SIZE {
+                    break;
+                }
             }
 
-            if byte >= DECODED_DATA_SIZE {
-                let mut size:usize = DECODED_DATA_SIZE - 1;
-
-                while decoded_data[size] == 0 {
-                    size -= 1;
-                }
-
-                pico_output.set_high(); //tell pico we are read to send data
-                spi.write(&decoded_data[0..size]).ok();
-                pico_output.set_low(); //tell pico we sent all the data
-
-                decoded_data = [0u8; DECODED_DATA_SIZE];
-                byte = 0;
-                preamble_size = 0;
+            if byte > 0 {
+                pico_output.set_high().ok(); // tell pico we are ready to send data
+                spi.write(&decoded_data[0..byte]).ok();
+                pico_output.set_low().ok(); // tell pico we sent all the data
             }
         }
     }
@@ -131,7 +128,7 @@ fn get_dcc_data<P: InputPin>(pin: &P, dwt: &DWT) -> bool {
 
     let end_cycles = dwt.cyccnt.read();
     let delta_cycles = end_cycles.wrapping_sub(start_cycles);
-    let delta_us = delta_cycles / 84;
+    let delta_us = delta_cycles / 72;
 
     return delta_us < 100;
 }
